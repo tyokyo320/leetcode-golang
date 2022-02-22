16583f7c22b275fa69ce5c5428d126e97231daab
zhangqiang <tyokyo320@gmail.com>
2022-02-22 16:49:00 +0900

    ✨ feat: update

---
+++ 36.valid-sudoku.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=36 lang=golang
+ *
+ * [36] Valid Sudoku
+ */
+
+// @lc code=start
+func isValidSudoku(board [][]byte) bool {
+    
+}
+// @lc code=end
+
---
+++ 73.set-matrix-zeroes.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=73 lang=golang
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+func setZeroes(matrix [][]int) {
+
+	// 用两个标记数组分别记录每一行和每一列是否有零出现
+	row := make([]bool, len(matrix))
+	col := make([]bool, len(matrix[0]))
+
+	for i, r := range matrix {
+		for j, v := range r {
+			if v == 0 {
+				row[i] = true
+				col[j] = true
+			}
+		}
+	}
+	for i, r := range matrix {
+		for j := range r {
+			if row[i] || col[j] {
+				r[j] = 0
+			}
+		}
+	}
+}
+
+// @lc code=end
+
