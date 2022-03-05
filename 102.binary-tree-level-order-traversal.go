1404b7150c19738b17609830ec5234e24b405c66
zhangqiang <tyokyo320@gmail.com>
2022-03-05 23:12:48 +0900

    ✨ feat: update

--- 102.binary-tree-level-order-traversal.go
+++ 102.binary-tree-level-order-traversal.go
@@ -14,7 +14,32 @@
  * }
  */
 func levelOrder(root *TreeNode) [][]int {
-	result := [][]int{}
+	if root == nil {
+		return [][]int{}
+	}
+
+	var result [][]int
+	queue := []*TreeNode{root}
+
+	// BFS
+	for i := 0; len(queue) > 0; i++ {
+		result = append(result, []int{})
+
+		for j := len(queue); j > 0; j-- {
+			if queue[0].Left != nil {
+				queue = append(queue, queue[0].Left)
+			}
+			if queue[0].Right != nil {
+				queue = append(queue, queue[0].Right)
+			}
+
+			result[i] = append(result[i], queue[0].Val)
+			// deque
+			queue = queue[1:]
+		}
+	}
+
+	return result
 }
 
 // @lc code=end
